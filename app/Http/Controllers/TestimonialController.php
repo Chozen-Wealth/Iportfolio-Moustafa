b493cf4dfdb749f17a7753f9919470dec375e65f
chozen <moustafa.eldehni@hotmail.com>
2025-08-21 14:03:41 +0200

    Tout les controllers terminés

--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class TestimonialController extends Controller
 {
-    //
+    public function index () {
+        $testimonials = Testimonial::all();
+        return  view("back.testimonials.index", compact("testimonials"));
+    }
+    public function create () {
+        return view("back.testimonials.create");
+    }
+    public function store (Request $request) {
+        $testimonial = new Testimonial();
+        $testimonial->comment = $request->comment;
+        $testimonial->img = $request->img;
+        $testimonial->name = $request->name;
+        $testimonial->position = $request->position;
+        $testimonial->save();
+
+        return redirect()->route("index_testimonials");
+    }
+    public function edit ($id) {
+        $testimonial = Testimonial::where("id", $id)->first();
+        return view("back.testimonials.edit", compact("testimonial"));
+    }
+    public function update ($id, Request $request) {
+        $testimonial = Testimonial::where("id", $id);
+        $testimonial->comment = $request->comment;
+        $testimonial->img = $request->img;
+        $testimonial->name = $request->name;
+        $testimonial->position = $request->position;
+        $testimonial->update();
+
+        return redirect()->route("index_testimonials");
+    }
+    public function destroy ($id) {
+        Testimonial::where("id", $id)->delete();
+        return redirect()->route("index_testimonials");
+    }
 }
