4c04de775ca044360c4783a73e4dae4e07d61294
Moustafa El Dehni <moustafa.eldehni@hotmail.com>
2025-08-25 04:18:41 +0200

    ajustement des boutons

--- resources/views/back/services/index.blade.php
+++ resources/views/back/services/index.blade.php
@@ -9,21 +9,21 @@
         </div>
         <div class="grille-services">
             @foreach ($services as $service)
-                <div class="d-flex gap-2" style="box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2); padding: 20px;border-radius: 10px;">
+                <div class="d-flex gap-3" style="box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2); padding: 20px;border-radius: 10px;">
                     <div class="bg-primary rounded-circle d-flex justify-content-center align-items-center" style="height: 40px; min-width: 40px">
                         {!! $service->icon !!}
                     </div>
                     <div>
                         <b>{{ $service->title }}</b>
                         <p>{{ $service->text }}</p>
-                    </div>
-                    <div class="d-flex align-items-center gap-2">
-                        <a class="btn btn-primary" href="{{ route("edit_services", $service->id) }}">Edit</a>
-                        <form action="{{ route("destroy_services", $service->id) }}" method="POST">
-                            @csrf
-                            @method("DELETE")
-                            <button class="btn btn-danger" type="submit">Delete</button>
-                        </form>
+                        <div class="d-flex align-items-center gap-2">
+                            <a class="btn btn-primary" href="{{ route("edit_services", $service->id) }}">Edit</a>
+                            <form action="{{ route("destroy_services", $service->id) }}" method="POST">
+                                @csrf
+                                @method("DELETE")
+                                <button class="btn btn-danger" type="submit">Delete</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             @endforeach
